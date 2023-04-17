a78686ffc16bdbca8d2cf2582eda5dd6681050fc
Shevhen <Shevhen9@gmail.com>
2023-04-16 18:51:27 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const app = express();
+
+const fs = require("fs")
+const path = require("path")
+const {json} = require("express");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+    app.get('/to',(req, res) =>{
+        fs.readFile("to.txt",{encoding: "utf-8"},(err, data)=>{
+            if(err){
+                res.json(err)
+            }
+            if(data){
+                res.json(data)
+            }
+        })
+    })
+
+app.listen(5100, () => {
+    // mongoose.connect("mongodb+srv://Yevhen:Yevhen@cluster0.ct2xzoh.mongodb.net/?retryWrites=true&w=majority");
+    // cronRunner()
+    console.log("Server!");
+});
