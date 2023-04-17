a1b8212d179fea2c42e66569b9f6141a8c5b1c1b
Shevhen <Shevhen9@gmail.com>
2023-04-16 19:42:06 -0700

    f

--- app.js
+++ app.js
@@ -4,21 +4,22 @@ const app = express();
 const fs = require("fs")
 const path = require("path")
 const {json} = require("express");
+const {users} = require("./users");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
     app.get('/to',(req, res) =>{
-        fs.readFile("to.txt",{encoding: "utf-8"},(err, data)=>{
-            if(err){
-                res.json(err)
-            }
-            if(data){
-                res.json(data)
-            }
-        })
+        // fs.readFile("to.txt",{encoding: "utf-8"},(err, data)=>{
+        //     if(err){
+        //         res.json(err)
+        //     }
+        //     if(data){
+        //         res.json(data)
+        //     }
+        // })
+        res.json(users)
     })
-
 app.listen(5100, () => {
     // mongoose.connect("mongodb+srv://Yevhen:Yevhen@cluster0.ct2xzoh.mongodb.net/?retryWrites=true&w=majority");
     // cronRunner()
