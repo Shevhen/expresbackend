54b65c12f807425ee48ea712f87aa1e3c7ecf5fd
Shevhen <Shevhen9@gmail.com>
2023-04-22 20:14:19 -0700

    f

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const app = express();
 // const {json} = require("express");
 const {users} = require("./users");
 
+const cors = require("cors")
+
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
